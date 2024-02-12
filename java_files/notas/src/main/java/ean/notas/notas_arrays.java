031949f151717f7aac05fb711d627200c6beb685
Johan H <jojhan_hernandez@hotmail.es>
2024-02-12 16:56:28 -0500

    Notas funcionando con arrays basicos

--- java_files/notas/src/main/java/ean/notas/notas_arrays.java
+++ java_files/notas/src/main/java/ean/notas/notas_arrays.java
@@ -5,30 +5,43 @@ import java.text.DecimalFormat;
 import java.util.*;
 public class notas_arrays {
     public static void main(String[] args){
-
-        float[] notas = {};
-
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa las notas");
-        float grade1 = sc.nextFloat();
-        float grade2 = sc.nextFloat();
-        float grade3 = sc.nextFloat();
-        float grade4 = sc.nextFloat();
-        System.out.println("Ahora ingresa los porcentajes de cada nota");
-        float per1= sc.nextFloat();
-        float per2= sc.nextFloat();
-        float per3= sc.nextFloat();
-        float per4= sc.nextFloat();
-        System.out.println("Ahora ingresa la nota que deseas sacar al final del curso");
-        float final_grade = sc.nextFloat();
+        System.out.println("Ingresa la nota que deseas sacar este semestre\n");
+        float goal = sc.nextFloat();
+        System.out.print("Ingresa la catidad de notas totales\n");
+        int notesQuantity = sc.nextInt();
+        float[] grades = new float[0];
+        for (int i = 0; i<notesQuantity; i++){
+            System.out.println("Ingresa la nota " + (i+1) + "\n");
+            float grade = sc.nextFloat();
+            int oldArrayLength = grades.length;
+            grades = addToArray(oldArrayLength, grades, grade);
+        }
+        System.out.println(Arrays.toString(grades));
 
-        find_grade(grade1, grade2, grade3, grade4, per1, per2, per3, per4, final_grade );
+        necessaryGrade(grades, goal, notesQuantity);
     }
-    static void find_grade(float grade1, float grade2, float grade3, float grade4, float per1, float per2, float per3, float per4, float final_grade) { // the way to find the grade u need to aprove your course
-        float per5 = 1 - (per1 + per2 + per3 + per4);
-        float goal_grade = (final_grade - (grade1*per1) - (grade2*per2) - (grade3*per3) - (grade4*per4)) / per5;
 
-        System.out.println("Necesitas sacar " + goal_grade + " para poder aprobar la materia con una nota final de: " + final_grade);
+    static float[] addToArray(int oldArrayLength, float[] oldArray, float newElement){
+        float[] newArray = new float[oldArrayLength+1];
+
+        for (int i = 0; i<oldArrayLength; i++){
+            newArray[i] = oldArray[i];
+        }
+        newArray[oldArrayLength] = newElement;
+        return newArray;
+    }
+
+    static float necessaryGrade(float[] grades, float goal, int notesQuantity){
+        DecimalFormat df = new DecimalFormat("#.00");
+        //float totalActualGrades = 0;
+        //for (int i = 0; i< notesQuantity; i++){
+        //  totalActualGrades = (grades[0]*(0.1)) - (grade2*per2) - (grade3*per3) - (grade4*per4);
+        //}
+
+        float gradeNeeded = (float) ((goal - (grades[0]*(0.1)) - (grades[1]*(0.15)) - (grades[2]*(0.2)) - (grades[3]*(0.25))) /  (0.3));
+
+        System.out.println("Necesitas sacar minimo: " + df.format(gradeNeeded));
+        return gradeNeeded;
     }
 }
