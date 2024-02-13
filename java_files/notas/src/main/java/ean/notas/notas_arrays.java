b3f6cb5ef5ce806554a521dbb91393991f313ed6
Johan H <jojhan_hernandez@hotmail.es>
2024-02-13 17:49:03 -0500

    Nota necesaria con arrays para una cantidad de estudiantes determinada por el usuario. Aun tiene comentarios de cosas anteriores.

--- java_files/notas/src/main/java/ean/notas/notas_arrays.java
+++ java_files/notas/src/main/java/ean/notas/notas_arrays.java
@@ -5,21 +5,34 @@ import java.text.DecimalFormat;
 import java.util.*;
 public class notas_arrays {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa la nota que deseas sacar este semestre\n");
-        float goal = sc.nextFloat();
-        System.out.print("Ingresa la catidad de notas totales\n");
-        int notesQuantity = sc.nextInt();
-        float[] grades = new float[0];
-        for (int i = 0; i<notesQuantity; i++){
-            System.out.println("Ingresa la nota " + (i+1) + "\n");
-            float grade = sc.nextFloat();
-            int oldArrayLength = grades.length;
-            grades = addToArray(oldArrayLength, grades, grade);
-        }
-        System.out.println(Arrays.toString(grades));
+        // float[][] student_grades = new float[10][4];
+        // student_grades[0] = new float[]{1, 2, 3, 4};
 
-        necessaryGrade(grades, goal, notesQuantity);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa la cantidad total de estudiantes -> ");
+        int total_students = sc.nextInt();
+
+        System.out.print("Ingresa la catidad de notas por estudiante -> ");
+        int notesQuantity = sc.nextInt();
+
+        float[][] student_grades = new float[total_students][notesQuantity + 1];
+
+        float goal = 60f; // minimum grade to approve
+        //float[] grades = new float[0];
+
+        for (int i = 0; i<total_students; i++){
+            float[] grades = new float[0];
+            for (int j = 0; j<notesQuantity; j++){
+                System.out.println("Ingresa la nota " + (j+1) + " para el estudiante " + (i+1) + " ");
+                float grade = sc.nextFloat();
+                int oldArrayLength = grades.length;
+                grades = addToArray(oldArrayLength, grades, grade);
+                student_grades[i] = grades;
+            }
+            System.out.println("La nota necesaria para que el estudiante número " + (i+1) + " saque 60 es: " + necessaryGrade(student_grades[i], 60, notesQuantity) + " \n");
+        }
     }
 
     static float[] addToArray(int oldArrayLength, float[] oldArray, float newElement){
@@ -32,7 +45,7 @@ public class notas_arrays {
         return newArray;
     }
 
-    static float necessaryGrade(float[] grades, float goal, int notesQuantity){
+    static String necessaryGrade(float[] grades, float goal, int notesQuantity){
         DecimalFormat df = new DecimalFormat("#.00");
         //float totalActualGrades = 0;
         //for (int i = 0; i< notesQuantity; i++){
@@ -41,7 +54,7 @@ public class notas_arrays {
 
         float gradeNeeded = (float) ((goal - (grades[0]*(0.1)) - (grades[1]*(0.15)) - (grades[2]*(0.2)) - (grades[3]*(0.25))) /  (0.3));
 
-        System.out.println("Necesitas sacar minimo: " + df.format(gradeNeeded));
-        return gradeNeeded;
+        //System.out.println("Necesitas sacar minimo: " + df.format(gradeNeeded));
+        return df.format(gradeNeeded);
     }
 }
