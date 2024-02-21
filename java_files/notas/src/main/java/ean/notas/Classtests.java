b73894aa9366eea6ff2aaa849654f88ef68b4a43
unknown <bryanrh0704@gmail.com>
2024-02-21 09:50:52 -0500

    Avances Bryan

---
+++ java_files/notas/src/main/java/ean/notas/Classtests.java
@@ -0,0 +1,24 @@
+package com.mycompany.classtests;
+
+import java.util.Scanner;
+
+public class Classtests {
+
+    public static void calcular() {
+        float aprobar = 60; // establecer la nota con la que se aprueba
+        float porcentajes[] = {0.3f, 0.2f, 0.2f, 0.3f}; // establecer los porcentajes
+        float dni[]; // identificación
+        // notas de cada corte
+        float corte1[]; 
+        float corte2[];
+        float corte3[];
+        float corte4[];
+       
+    }
+
+    public static void float calcularPromedio() {
+        float[] promEst = new float[];
+        
+    }
+
+}
